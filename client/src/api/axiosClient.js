b648a95e209f6802363a681aaeeb133d84f0c0e9
raghav <raghavjaiswal0000@gmail.com>
2025-03-15 21:26:36 +0530

    production

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'http://127.0.0.1:5000/api/v1/'
+const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1/'
 
 const axiosClient = axios.create({
   baseURL: baseUrl,
