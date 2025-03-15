bb77ee755824166de8c8cec1fa073c3f2a4d465f
raghav <raghavjaiswal0000@gmail.com>
2025-03-16 00:16:19 +0530

    fixed base url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ import { useState } from "react"
 const Home = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)//
 
   const createBoard = async () => {
     setLoading(true)
